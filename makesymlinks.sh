d84e9db19f9e22211375c9c3428e2faa645aead1
Håvard Flaget Aasen <haavard_aasen@yahoo.no>
2021-07-14 19:44:52 +0200

    Double quote and put braces around variables.
    'shellcheck' warned against globbing and word splitting. Also put braces
    around each variable.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -13,20 +13,20 @@ files="bashrc vimrc lintianrc dput.cf gbp.conf"    # list of files/folders to sy
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating ${olddir} for backup of any existing dotfiles in ~"
+mkdir -p ${olddir}
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir || exit
+echo "Changing to the ${dir} directory"
+cd ${dir} || exit
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+for file in ${files}; do
+    echo "Moving any existing dotfiles from ~ to ${olddir}"
+    mv ~/."${file}" "${olddir}"
+    echo "Creating symlink to ${file} in home directory."
+    ln -s "${dir}/${file}" ~/."${file}"
 done
 
